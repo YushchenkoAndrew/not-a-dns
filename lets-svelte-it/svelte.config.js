d6f7cb8c720d796f2aac360f5d47e87b81362d21
Andrew Y <Andrew10122000@gmail.com>
2022-03-08 18:53:03 +0200

    Add tailwind to svelte

--- lets-svelte-it/svelte.config.js
+++ lets-svelte-it/svelte.config.js
@@ -1,20 +1,30 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import path from "path";
+import adapter from "@sveltejs/adapter-auto";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+export const config = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
+    // Override http methods in the Todo forms
+    methodOverride: {
+      allowed: ["PATCH", "DELETE"],
+    },
+
+    vite: {
+      resolve: {
+        alias: {
+          // these are the aliases and paths to them
+          $components: path.resolve("./src/components"),
+        },
+      },
+    },
+  },
 };
 
 export default config;
